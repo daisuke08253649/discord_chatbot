16a4fa289ee8a19785fb3a64cdbcbea92db2ba06
yamadai <yamadai08253649@gmail.com>
2025-04-09 15:54:20 +0900

    app.py修正

--- app.py
+++ app.py
@@ -1,12 +1,29 @@
-from flask import Flask
-from threading import Thread
+import discord
+import os
+from dotenv import load_dotenv
+from keep_alive import keep_alive
 
-app = Flask(__name__)
+load_dotenv()
 
-@app.route('/')
-def run():
-    app.run(host='0.0.0.0', port=8080, debug=False)
+TOKEN = os.getenv("DISCORD_TOKEN")
 
-def keep_alive():
-    t = Thread(target=run, daemon=True)
-    t.start()
+client = discord.Client(intents=discord.Intents.default())
+
+@client.event
+async def on_ready():
+    print('ログインしました')
+
+@client.event
+async def on_message(message):
+    # 自身が送信したメッセージには反応しない
+    if message.author == client.user:
+        return
+
+    # ユーザーからのメンションを受け取った場合、あらかじめ用意された配列からランダムに返信を返す
+    if client.user in message.mentions:
+        await message.channel.send(message.content)
+
+
+# Web サーバの立ち上げ
+keep_alive()
+client.run(TOKEN)
--- message.py
+++ message.py
@@ -1,51 +1,51 @@
-import os
-import discord
-import asyncio
-# from wiki_assistant import Chatgpt
-# from wiki_embeddings import WikiContent
-from dotenv import load_dotenv
-from datetime import datetime
-from app import keep_alive
+# import os
+# import discord
+# import asyncio
+# # from wiki_assistant import Chatgpt
+# # from wiki_embeddings import WikiContent
+# from dotenv import load_dotenv
+# from datetime import datetime
+# from app import keep_alive
 
-load_dotenv()
+# load_dotenv()
 
-token = os.getenv("DISCORD_TOKEN")
+# token = os.getenv("DISCORD_TOKEN")
 
-intents = discord.Intents.default()
-client = discord.Client(intents=intents)
-
-
-## Botの起動
-@client.event
-async def on_ready():
-    print("Start")
+# intents = discord.Intents.default()
+# client = discord.Client(intents=intents)
 
 
-# メッセージ受信時の処理
-@client.event
-async def on_message(message):
-    # Bot自身のメッセージの場合は無視
-    if message.author == client.user:
-        return
+# ## Botの起動
+# @client.event
+# async def on_ready():
+#     print("Start")
+
+
+# # メッセージ受信時の処理
+# @client.event
+# async def on_message(message):
+#     # Bot自身のメッセージの場合は無視
+#     if message.author == client.user:
+#         return
     
-    if client.user in message.mentions:
-        print(message.content)
-        # chatgpt = Chatgpt(message.content)
-        # resMessage = chatgpt.resChatgpt()
-        await message.channel.send("resMessage")
+#     if client.user in message.mentions:
+#         print(message.content)
+#         # chatgpt = Chatgpt(message.content)
+#         # resMessage = chatgpt.resChatgpt()
+#         await message.channel.send("resMessage")
 
 
 
 
-if __name__ == '__main__':
-    print("ベクトルDBの状態を確認中...")
-    # vectorstore = WikiContent.get_vectorstore()
-    # vectorstoreをChatgptクラスに設定
-    # Chatgpt.set_vectorstore(vectorstore)
-    print("ベクトルDBの準備完了")
+# if __name__ == '__main__':
+#     print("ベクトルDBの状態を確認中...")
+#     # vectorstore = WikiContent.get_vectorstore()
+#     # vectorstoreをChatgptクラスに設定
+#     # Chatgpt.set_vectorstore(vectorstore)
+#     print("ベクトルDBの準備完了")
 
-    # Web サーバの立ち上げ
-    keep_alive()
+#     # Web サーバの立ち上げ
+#     keep_alive()
     
-    # Bot起動
-    client.run(token)
+#     # Bot起動
+#     client.run(token)
